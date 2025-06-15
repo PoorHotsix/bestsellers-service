496acce3505995790d11011608e1bdf8a52cde8c
PoorHotsix <majch98@gmail.com>
2025-06-15 23:30:34 +0900

    프로젝트 파일 초기화

---
+++ src/test/java/com/inkcloud/bestsellers_service/BestsellersServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inkcloud.bestsellers_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BestsellersServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
