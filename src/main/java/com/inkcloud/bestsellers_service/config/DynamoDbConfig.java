5a4c921e0f4c9a7c9da68f195326e71020cd3861
PoorHotsix <majch98@gmail.com>
2025-06-18 19:00:44 +0900

    Modify DynamoDbConfig

--- src/main/java/com/inkcloud/bestsellers_service/config/DynamoDbConfig.java
+++ src/main/java/com/inkcloud/bestsellers_service/config/DynamoDbConfig.java
@@ -3,7 +3,7 @@ package com.inkcloud.bestsellers_service.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import software.amazon.awssdk.auth.credentials.EnvironmentVariableCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
 import software.amazon.awssdk.enhanced.dynamodb.DynamoDbEnhancedClient;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
@@ -15,7 +15,7 @@ public class DynamoDbConfig {
     public DynamoDbClient dynamoDbClient() {
         return DynamoDbClient.builder()
             .region(Region.AP_NORTHEAST_2)
-            .credentialsProvider(EnvironmentVariableCredentialsProvider.create())
+            .credentialsProvider(DefaultCredentialsProvider.create())
             .build();
     }
 
